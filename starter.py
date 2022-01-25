f4567c0665c61c86ceb6d5d6df64bb18eccdd663
Andrew Jolley <64013644+andrew-jolley@users.noreply.github.com>
2022-01-25 12:44:05 +0000

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,7 @@
+userInput = int(input("Enter your number input: "))
+userInput = userInput * 2
+print(userInput)
+while userInput >= 0:
+    userInput = int(input("Enter your number input: "))
+    userInput = userInput * 2
+    print(userInput)
