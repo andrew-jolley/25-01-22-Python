60cf1850c04434215d22ae807fb7f01dcedcc25f
Andrew Jolley <64013644+andrew-jolley@users.noreply.github.com>
2022-01-26 14:35:21 +0000

    Update fibonacci sequence.py

--- 26-01-22/fibonacci sequence.py
+++ 26-01-22/fibonacci sequence.py
@@ -2,18 +2,11 @@ noTerms = 10
 #noTerms = int(input("How many terms? "))
 
 num1, num2 = 0, 1
-count = 0
-
-if noTerms <= 0:
-   print("Please enter a positive integer")
-elif noTerms == 1:
-   print("Fibonacci sequence upto",noTerms,":")
+goes = 0
+print("Fibonacci sequence:")
+while goes < noTerms:
    print(num1)
-else:
-   print("Fibonacci sequence:")
-   while count < noTerms:
-       print(num1)
-       nth = num1 + num2
-       num1 = num2
-       num2 = nth
-       count += 1
+   nTHTRM = num1 + num2
+   num1 = num2
+   num2 = nTHTRM
+   goes += 1
