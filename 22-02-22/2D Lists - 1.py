97a41120e6acdd14772dbf4e92d2d58c4330b62f
Andrew Jolley <64013644+andrew-jolley@users.noreply.github.com>
2022-02-22 10:22:15 +0000

    Add files via upload

---
+++ 22-02-22/2D Lists - 1.py
@@ -0,0 +1,8 @@
+two_dim_list = [["r1c1", "r1c2", "r1c3"],["r2c1", "r2c2", "r2c3"]]
+print("***************")
+print("Table is: ", two_dim_list)
+print("***************")
+print("Row 1 is: ", two_dim_list[0])
+print("***************")
+print("Row 1, Column 2 is: ", two_dim_list[0][1])
+print("***************")
